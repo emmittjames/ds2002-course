ca40719dc19a58280c2be6c2e2f88e466ee0f11f
emmittjames <emmittjames1@gmail.com>
2024-02-20 15:52:50 -0500

    madlib script

---
+++ mywork/03-scripting/madlib.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+clear
+echo "Let's build a mad-lib!"
+
+read -p "1. Please give me an adjective: " ADJ1
+read -p "2. Please give me an noun: " NOUN1
+read -p "3. Please give me an noun: " NOUN2
+read -p "4. Please give me an verb: " VERB1
+read -p "5. Please give me an adjective: " ADJ2
+read -p "6. Please give me an noun: " NOUN3
+read -p "7. Please give me an adverb: " ADV1
+read -p "8. Please give me an verb: " VERB2
+
+echo "Once upon a time, in a land far away, there lived a $ADJ1 $NOUN1 named $NOUN2. $NOUN2 loved to $VERB1 all day long. One day, a $ADJ2 $NOUN3 approached $NOUN2 and said, "You $ADV1 $VERB2!" From that day on, $NOUN2 $VERB2 $ADV1 ever after."
+

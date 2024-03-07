8d8d56a7b44b242a6e3c53bb58926bfacf8bfb3a
student <gwu8ek@virginia.edu>
2024-03-06 21:20:04 -0800

    fixed python script

--- mywork/lab4/script.py
+++ mywork/lab4/script.py
@@ -29,8 +29,8 @@ def upload_to_s3_and_generate_presigned_url(bucket_name, file_path, expires_in):
     return presigned_url
 
 bucket_name = 'ds2002-gwu8ek'
-url = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fdatascience.virginia.edu%2Fnews%2Fuva-named-top-20-public-universities-high-paying-data-science-jobs&psig=AOvVaw251RjrR-Xz7Hng5yqC8SdD&ust=1709760250311000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCMj809eL3oQDFQAAAAAdAAAAABAD'
-file_path = 'rotunda.png'
+url = 'https://api.dsi.virginia.edu/sites/default/files/styles/scale_lg/public/2023-05/rotunda_march_2018_ss_header_3-2_1%20%281%29.jpg'
+file_path = 'rotunda.jpg'
 expires_in = 604800 
 
 fetch_and_save_file(url, file_path)
@@ -38,3 +38,4 @@ fetch_and_save_file(url, file_path)
 presigned_url = upload_to_s3_and_generate_presigned_url(bucket_name, file_path, expires_in)
 
 print("Presigned URL:", presigned_url)
+
